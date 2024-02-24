7eb1d4ce3b6951d40d202e8abeb975e92b7c39e8
KaviHT <kaviht1001@gmail.com>
2024-02-25 00:07:23 +0530

    Add database backup button and confirmation

--- src/main/java/com/example/employeeattendancesystem/Controllers/DashboardController.java
+++ src/main/java/com/example/employeeattendancesystem/Controllers/DashboardController.java
@@ -5,13 +5,20 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
+import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 
+import java.io.File;
 import java.io.IOException;
+import java.time.LocalDate;
+import java.util.Optional;
+
 
 public class DashboardController {
-    public Button sitesBtn, employeeBtn, attendanceBtn, logoutBtn;
+    public Button sitesBtn, employeeBtn, attendanceBtn, logoutBtn, backupBtn;
     private Stage stage;
     private Scene scene;
     private Parent root;
@@ -51,4 +58,55 @@ public class DashboardController {
         stage.show();
     }
 
+    public void backupDatabase() {
+        // Creating a confirmation alert to get the confirmation form the user
+        Alert backupConfirm = new Alert(Alert.AlertType.CONFIRMATION);
+        backupConfirm.setTitle("Confirmation");
+        backupConfirm.setHeaderText("Backup System Database");
+        backupConfirm.setContentText("Do you want to save a backup of the database?");
+
+        ButtonType yesButton = new ButtonType("Yes");
+        ButtonType noButton = new ButtonType("No");
+        backupConfirm.getButtonTypes().setAll(yesButton, noButton);
+
+        Optional<ButtonType> result = backupConfirm.showAndWait();
+
+        // Handle the user's response
+        if (result.isPresent() && result.get() == yesButton) {  // User clicked OK
+            LocalDate backupDate = LocalDate.now();
+
+            // ---------------------------------------------------------------
+            // Implementation for getting backup from the Database
+            // ---------------------------------------------------------------
+
+            filePathSelection(backupDate);
+        } else {
+            // User clicked Cancel or closed the alert
+        }
+    }
+
+    private void filePathSelection(LocalDate backupDate) {
+        // Open the file path selection window
+        FileChooser fileChooser = new FileChooser();
+        // Set extension filter for text files
+        FileChooser.ExtensionFilter extFilter = new FileChooser.ExtensionFilter("CSV files (*.csv)", "*.csv");
+        fileChooser.getExtensionFilters().add(extFilter);
+
+        // Set a default file name
+        fileChooser.setInitialFileName(backupDate + "_Backup" + ".csv");
+
+        // Show save file dialog
+        Stage stage = (Stage) backupBtn.getScene().getWindow();
+        fileChooser.setTitle("Save Backup");
+        File file = fileChooser.showSaveDialog(stage);
+
+        if (file != null) {
+
+            // -----------------------------------------------------
+            // Implement file saving logic here using file.getPath()
+            // -----------------------------------------------------
+
+            System.out.println("Save path selected: " + file.getPath());
+        }
+    }
 }
