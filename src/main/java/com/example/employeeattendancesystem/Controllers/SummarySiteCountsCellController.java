501f1c93a0c68a41c32de0b4ceb016f2ff807d02
KaviHT <kaviht1001@gmail.com>
2024-04-02 23:50:43 +0530

    Setup and create total attendance count UI

---
+++ src/main/java/com/example/employeeattendancesystem/Controllers/SummarySiteCountsCellController.java
@@ -0,0 +1,7 @@
+package com.example.employeeattendancesystem.Controllers;
+
+import javafx.scene.control.Label;
+
+public class SummarySiteCountsCellController {
+    public Label presentCountLbl, leaveCountLbl;
+}
